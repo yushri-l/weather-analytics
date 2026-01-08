517a83355773b03b4da7e28da718fc6a13f0eb7a
yushri <yushri.ml@gmail.com>
2026-01-08 17:11:03 +0530

    feat: protect weather API with Auth0 JWT authentication

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,4 +12,12 @@ app.use(express.json());
 
 app.use("/api/weather", weatherRoutes);
 
+app.use((err, req, res, next) => {
+  if (err.name === "UnauthorizedError") {
+    return res.status(401).json({ message: "Invalid or missing token" });
+  }
+  next(err);
+});
+
+
 export default app;
